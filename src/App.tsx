57a017f0e4c9fef75bfa67e69d31384a6c287221
grigri201 <144085936+grigri201@users.noreply.github.com>
2025-05-17 16:29:48 +0800

    chore: migrate to react+vite with ts

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CanvasEffects from './components/CanvasEffects';
+import AudioPlayer from './components/AudioPlayer';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <CanvasEffects />
+      <AudioPlayer />
+    </>
+  );
+};
+
+export default App;
